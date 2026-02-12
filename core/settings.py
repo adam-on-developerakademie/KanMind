1dbb80679faf0bd114e433497c0118cae434fbd3
adamp <adam.piskorek@gmx.de>
2026-02-12 03:14:09 +0100

    Update CORS settings and enable tasks API URL routing

--- core/settings.py
+++ core/settings.py
@@ -99,6 +99,14 @@ TEMPLATES = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5000",  
     "http://127.0.0.1:5000",
+    "http://localhost:5500",  
+    "http://127.0.0.1:5500",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+    "http://localhost:8080",
+    "http://127.0.0.1:8080",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
